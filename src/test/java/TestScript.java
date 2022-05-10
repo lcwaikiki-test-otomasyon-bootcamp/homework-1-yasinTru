5f9a4197403d5f7ac8787321b701ff4b731ed95d
yasin dogru <yasindogru61@gmail.com>
2022-05-11 01:40:11 +0300

    kod yazımları refaktör edildi.

--- src/test/java/TestScript.java
+++ src/test/java/TestScript.java
@@ -13,49 +13,49 @@ public class TestScript {
     public static void main(String[] args) throws InterruptedException
     {
 
-        //Setup adımları.
+        //Setup things.
+
+        //Chrome and Webdriver setups.
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled"); //removes the detection
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"}); //removes the banner
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
-        Actions actions= new Actions(driver);   //giriş sayfasındaki kulllanıcı logosunu hover'lamak için.
+        Actions actions= new Actions(driver);   //to hover the user logo on the login page.
 
-        //Siteye giriş işlemleri.
-        driver.navigate().to("https://www.lcwaikiki.com/tr-TR/TR/");
-        driver.manage().window().maximize();
+        //into the Main page.
+        driver.navigate().to("https://www.lcwaikiki.com/tr-TR/TR/");  //Navigating to the page.
+        driver.manage().window().maximize();   //fullscreen.
         Thread.sleep(2000);
-        WebElement girisLogo= driver.findElement(By.xpath("//header/div[2]/div[3]/div[1]/span[1]/div[1]/a[1]"));
-        actions.moveToElement(girisLogo).perform();
-
-        WebElement mainGirisButon= driver.findElement(By.cssSelector(".cart-action__btn.cart-action__btn--bg-white"));
+        WebElement mainLogo= driver.findElement(By.xpath("//header/div[2]/div[3]/div[1]/span[1]/div[1]/a[1]"));    //logo element for Register process.
+        actions.moveToElement(mainLogo).perform();     //logo's hover.
+        WebElement mainLoginButton= driver.findElement(By.cssSelector(".cart-action__btn.cart-action__btn--bg-white"));
         Thread.sleep(3000);
-        System.out.println("main butonu");
-        mainGirisButon.click();
+        mainLoginButton.click();    //clicking on the button.
 
 
-        //Üye ol sayfası işlemleri
+        //Register steps.
         WebElement ePostaInput= driver.findElement(By.cssSelector("#RegisterFormView_RegisterEmail"));
         ePostaInput.sendKeys("kesinlikleyanlis");
-        WebElement sifreInput= driver.findElement(By.cssSelector("#RegisterFormView_Password"));
-        sifreInput.sendKeys("yanlisTabii");
-        WebElement telNo= driver.findElement(By.cssSelector("#RegisterPhoneNumberTR"));
-        telNo.sendKeys("1111111111");
+        WebElement passwordInput= driver.findElement(By.cssSelector("#RegisterFormView_Password"));
+        passwordInput.sendKeys("yanlisTabii");
+        WebElement phoneNumber= driver.findElement(By.cssSelector("#RegisterPhoneNumberTR"));
+        phoneNumber.sendKeys("1111111111");
         WebElement privacyLabel= driver.findElement(By.cssSelector("label[for='member-privacy-approve'] ins[class='iCheck-helper']"));
         privacyLabel.click();
-        // login-form__button.login-form__button--bg-blue    --> Buton ama cssSelector
-        // driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();     --> Buton ama xpath
+        // login-form__button.login-form__button--bg-blue    --> Button but cssSelector
+        // driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();     --> Button but xpath
        // driver.findElement(By.cssSelector(".button.bc-blue.register-button-link.submit-button")).click();
         Thread.sleep(2000);
 
-        //Console çıktıları.
-        String mailHataMesaji=driver.findElement(By.cssSelector("#RegisterFormView_RegisterEmail-error")).getText();
-        System.out.println(mailHataMesaji);
-        String sifreHataMesaji=driver.findElement(By.id("RegisterFormView_Password-error")).getText();
-        System.out.println(sifreHataMesaji);
-        String telHataMesaji=driver.findElement(By.id("RegisterPhoneNumberTR-error")).getText();
-        System.out.println(telHataMesaji);
+        //Writing error messages to the console.
+        String mailErrorMessage=driver.findElement(By.cssSelector("#RegisterFormView_RegisterEmail-error")).getText();
+        System.out.println(mailErrorMessage);
+        String passErrorMessage=driver.findElement(By.id("RegisterFormView_Password-error")).getText();
+        System.out.println(passErrorMessage);
+        String phoneErrorMessage=driver.findElement(By.id("RegisterPhoneNumberTR-error")).getText();
+        System.out.println(phoneErrorMessage);
 
     }
 }
